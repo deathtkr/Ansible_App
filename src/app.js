1dceefe6d7cdc44eee66e58587d1acf20e7a11da
Deathtkr <adityasaxena34244@gmail.com>
2024-09-18 14:55:56 +0530

    dd

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 4220;
 console.log('Database Host:', process.env.DB_HOST);
 // MySQL connection setup
 const connection = mysql.createConnection({
