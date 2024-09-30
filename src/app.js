8e103d48b57b3d17fb3a32f45a46c4bc759e29e1
deathtkr <adityasaxena34244@gmail.com>
2024-09-30 18:14:54 +0530

    new

--- src/app.js
+++ src/app.js
@@ -7,13 +7,13 @@ const port = 4330;
 console.log('Database Host:', process.env.DB_HOST);
 // MySQL connection setup
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,  // Use environment ddcjjjjd
+  host: process.env.DB_HOST,  // Use environme
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
-console.log('Database Host:', process.env.DB_HOST);  // Log the DB_HOST val
+console.log('Database Host:', process.env.DB_HOST);  // Log the DB_HOSd
 
 connection.connect((err) => {
   if (err) {
